998ee7b087b2b6c33fa0a1eef1a8899990c1a845
Philipp Nowinski <mail@pno.dev>
2023-05-03 11:10:08 +0200

    feat: move setNodeVersion to utils

--- core/cli.sh
+++ core/cli.sh
@@ -6,26 +6,6 @@ if [ -e "./.jimrc" ]; then
   source ./.jimrc
 fi
 
-function setNodeVersion() {
-  # abort if no NODE_VERSION has been set
-  if [ ! -v NODE_VERSION ]; then
-    return
-  fi
-
-  NVM_PATH=~/.nvm/nvm.sh
-  if [[ -f "$NVM_PATH" ]]; then
-    source "$NVM_PATH"
-    nvm use --delete-prefix "${NODE_VERSION}" >/dev/null
-    _log "Using node $(node -v)" $COLOR_YELLOW
-  else
-    echo ""
-    _log " ⚠️  It looks like the nvm command is not installed on your system." $COLOR_RED
-    _log " ⚠️  Setting the node version automatically depends on this command." $COLOR_RED
-    _log " ⚠️  Please visit https://github.com/nvm-sh/nvm to find out how to install it on your system." $COLOR_RED
-    echo ""
-  fi
-}
-
 COMMAND=$1
 
 if [ "$COMMAND" == "shortlist" ]; then
--- core/utils.sh
+++ core/utils.sh
@@ -13,3 +13,23 @@ function _log() {
   fi
   echo -e "$COLOR_DIMMED[jim]$COLOR_RESET $FORMAT_START$1$FORMAT_END"
 }
+
+function setNodeVersion() {
+  # abort if no NODE_VERSION has been set
+  if [ ! -v NODE_VERSION ]; then
+    return
+  fi
+
+  NVM_PATH=~/.nvm/nvm.sh
+  if [[ -f "$NVM_PATH" ]]; then
+    source "$NVM_PATH"
+    nvm use --delete-prefix "${NODE_VERSION}" >/dev/null
+    _log "Using node $(node -v)" $COLOR_YELLOW
+  else
+    echo ""
+    _log " ⚠️  It looks like the nvm command is not installed on your system." $COLOR_RED
+    _log " ⚠️  Setting the node version automatically depends on this command." $COLOR_RED
+    _log " ⚠️  Please visit https://github.com/nvm-sh/nvm to find out how to install it on your system." $COLOR_RED
+    echo ""
+  fi
+}
